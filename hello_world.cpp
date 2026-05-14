d3566242fb421c8436f5fe1f3897b254fbbf1b37
marykos0505 <TheKittenMay@yandex.ru>
2026-05-14 10:33:59 +0300

    hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,1 +1,7 @@
-#include <iostream>\n#include <string>\nusing namespace std;\nint main() { string name; cin >> name; cout << "Hello world from @" << name << endl; return 0; }
+#include <iostream>
+#include <string>
+using namespace std;
+int main() 
+{ string name; 
+ cin >> name; 
+ cout << "Hello world from @" << name << endl; return 0; }
