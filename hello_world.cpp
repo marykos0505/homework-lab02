5b434fb2f9a663af523b581e2b0f37a0534f9073
marykos0505 <TheKittenMay@yandex.ru>
2026-05-14 07:34:24 +0000

    Refactor: removed using namespace std and added std:: prefix

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,1 +1,10 @@
-#include <iostream>\n#include <string>\nusing namespace std;\nint main() { string name; cin >> name; cout << "Hello world from @" << name << endl; return 0; }
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string name;
+    std::cout << "Enter your name: ";
+    std::cin >> name;
+    std::cout << "Hello world from @" << name << std::endl;
+    return 0;
+}
